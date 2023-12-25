8b494aaa48c6d9893b6aef9c99380148e23ea6a4
Sarthak Jaybhay <sarthakjaybhay2221@gmail.com>
2023-12-26 00:50:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// server/index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Serve static files from the client/build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Serve the React app on all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
